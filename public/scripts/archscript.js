d40a95eb38d7647d2b5a14234bde8c54800e40ad
Arshynbeek <justarshynbek@gmail.com>
2023-10-17 09:24:35 +0600

    remaking

---
+++ public/scripts/archscript.js
@@ -0,0 +1,32 @@
+let drop = false;
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  if (drop == false) {
+    document.querySelector(".menu-body").style.height = "400px";
+    drop = true;
+  } else {
+    document.querySelector(".menu-body").style.height = "0px";
+    drop = false;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+  anchorLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const targetId = link.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        const scrollPosition = targetElement.offsetTop - 90;
+
+        window.scrollTo({
+          top: scrollPosition,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
